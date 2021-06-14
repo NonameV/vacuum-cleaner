e1d54009466530a646edd0a949897c6c47191b61
Nikita <nonamekuden@gmail.com>
2021-06-14 17:00:21 +0300

    начало работы с сайтом

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import './style.scss';
+import Parallax from 'parallax-js';
+
+// var scene = document.querySelector('.vacuum-wrapper');
+// var parallaxInstance = new Parallax(scene);
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    // context: '/src',
+    entry: {
+        app: './src/app.js',
+        polyfill: 'babel-polyfill'
+    },
+    plugins: [
+        new htmlWebpackPlugin({ template: './src/index.html' }),
+        new CleanWebpackPlugin()
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'src'),
+        compress: true,
+        port: 9000,
+        hot: true,
+        host: 'localhost',
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                }
+
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" }
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" },
+                    { loader: "postcss-loader" },
+                    { loader: "sass-loader" }
+                ],
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: ['file-loader']
+            }
+
+        ]
+    }
+}
